4a01c3b0f1cdb391edb1828aa18c213682197148
Johan Kämpe <kampe.johan@gmail.com>
2017-03-20 13:54:55 +0100

    Step counter

--- Kod/VibSensor/VibSensor.ino
+++ Kod/VibSensor/VibSensor.ino
@@ -1,14 +1,23 @@
-/*   Vibration sensor  */
-#define VIBRATION_TRIGGER 10
+
+#define VIBRATION_TRIGGER 20
+#define STEPDELAY 300
 const int VIB_SENSOR_PIN = A0;
+
+int steps = 0;
+unsigned long timer = 0;
 
 void setup(){
     Serial.begin(9600);
 }
 
 void loop(){
-    float vibrationData =
-      (float)analogRead(VIB_SENSOR_PIN) * 0.5;
-    Serial.println(vibrationData);
-    delay(10);
+    float vibrationData = (float)analogRead(VIB_SENSOR_PIN) * 0.2;
+
+    if((vibrationData >= VIBRATION_TRIGGER) &&
+        (millis() - timer) > STEPDELAY) {
+            steps++;
+            Serial.println(vibrationData);
+            Serial.println(steps);
+            timer = millis();
+    }
 }
