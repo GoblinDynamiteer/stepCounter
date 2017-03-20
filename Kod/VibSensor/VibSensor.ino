e3e3b053597a00fa048816278717896d7e706c5f
Johan Kämpe <kampe.johan@gmail.com>
2017-03-20 13:24:02 +0100

    Vibration sensor test

---
+++ Kod/VibSensor/VibSensor.ino
@@ -0,0 +1,14 @@
+/*   Vibration sensor  */
+#define VIBRATION_TRIGGER 10
+const int VIB_SENSOR_PIN = A0;
+
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+    float vibrationData =
+      (float)analogRead(VIB_SENSOR_PIN) * 0.5;
+    Serial.println(vibrationData);
+    delay(10);
+}
