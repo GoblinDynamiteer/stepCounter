f2b1a6aedd2b25e4beda9a9ab224b01d6de4d543
Johan Kämpe <kampe.johan@gmail.com>
2017-03-20 12:07:46 +0100

    Updated

--- Kod/MPU/mpu_6050_data/mpu_6050_data.ino
+++ Kod/MPU/mpu_6050_data/mpu_6050_data.ino
@@ -4,7 +4,8 @@
 // Public Domain
 #include<Wire.h>
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int16_t AcX,AcY,AcZ,combined;
+int steps = 0, offset = 8100;
 void setup(){
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -21,16 +22,7 @@ void loop(){
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  Serial.print("AcX = "); Serial.print(AcX);
-  Serial.print(" | AcY = "); Serial.print(AcY);
-  Serial.print(" | AcZ = "); Serial.print(AcZ);
-  Serial.print(" | GyX = "); Serial.print(GyX);
-  Serial.print(" | GyY = "); Serial.print(GyY);
-  Serial.print(" | GyZ = "); Serial.println(GyZ);
-  Serial.println(GyX);
+  combined = ((AcX+AcY+AcZ) / 3) - offset;
+  Serial.println(combined);
   delay(10);
 }
