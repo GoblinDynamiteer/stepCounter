a3165dac73d71ee4388a3cefa9fc07a785f8d3ab
Unknown <dennisbunne92@gmail.com>
2017-03-20 14:35:23 +0100

    Kopplingschema mpu 5V - 2.9V + test kod mpu attiny

---
+++ Kod/mpu test attiny/mpu_test/mpu_test/Debug/main.d
@@ -0,0 +1,46 @@
+main.d main.o: .././main.c \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ D:\Programmering\7.0\Packs\atmel\ATtiny_DFP\1.1.102\include/avr/iotn85.h \
+ D:\Programmering\7.0\Packs\atmel\ATtiny_DFP\1.1.102\include/avr/iotnx5.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+D:\Programmering\7.0\Packs\atmel\ATtiny_DFP\1.1.102\include/avr/iotn85.h:
+
+D:\Programmering\7.0\Packs\atmel\ATtiny_DFP\1.1.102\include/avr/iotnx5.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\programmering\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
