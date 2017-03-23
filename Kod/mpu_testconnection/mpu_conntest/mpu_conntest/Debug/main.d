f8edae663e2c4dbadce99f2bf534763e18506e2b
Johan Kämpe <kampe.johan@gmail.com>
2017-03-23 13:47:47 +0100

    Egen funktion för att hämta acc data

--- Kod/mpu_testconnection/mpu_conntest/mpu_conntest/Debug/main.d
+++ Kod/mpu_testconnection/mpu_conntest/mpu_conntest/Debug/main.d
@@ -14,10 +14,11 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ .././i2chw/i2cmaster.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- .././mpu6050/mpu6050.h .././mpu6050/mpu6050registers.h .././uart/uart.h
+ .././mpu6050/mpu6050.h .././mpu6050/mpu6050registers.h
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
 
@@ -49,6 +50,8 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
 
+.././i2chw/i2cmaster.h:
+
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
@@ -58,5 +61,3 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 .././mpu6050/mpu6050.h:
 
 .././mpu6050/mpu6050registers.h:
-
-.././uart/uart.h:
--- Kod/mpu_testconnection/mpu_conntest/mpu_conntest/main.c
+++ Kod/mpu_testconnection/mpu_conntest/mpu_conntest/main.c
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
+#include "i2chw/i2cmaster.h"
 
 #define F_CPU 1000000UL
 #include <util/delay.h>
@@ -9,40 +10,20 @@
 
 #include "mpu6050/mpu6050.h"
 
-#define UART_BAUD_RATE 57600
-#include "uart/uart.h"
+//#define X MPU6050_RA_ACCEL_XOUT_H //Adress för accelerationdata X
+//#define Y MPU6050_RA_ACCEL_YOUT_H //Adress för accelerationdata Y
+//#define Z MPU6050_RA_ACCEL_ZOUT_H //Adress för accelerationdata Z
+
+#define X 0x3B //Adress för accelerationdata X
+#define Y 0x3D //Adress för accelerationdata Y
+#define Z 0x3F //Adress för accelerationdata Z
+
+double getAcc(int addr);
+
 
 int main(void) {
 	DDRB = PB0;
-	#if MPU6050_GETATTITUDE == 0
-	int16_t ax = 0;
-	int16_t ay = 0;
-	int16_t az = 0;
-	int16_t gx = 0;
-	int16_t gy = 0;
-	int16_t gz = 0;
-	double axg = 0;
-	double ayg = 0;
-	double azg = 0;
-	double gxds = 0;
-	double gyds = 0;
-	double gzds = 0;
-	#endif
-
-	#if MPU6050_GETATTITUDE == 1 || MPU6050_GETATTITUDE == 2
-	long *ptr = 0;
-	double qw = 1.0f;
-	double qx = 0.0f;
-	double qy = 0.0f;
-	double qz = 0.0f;
-	double roll = 0.0f;
-	double pitch = 0.0f;
-	double yaw = 0.0f;
-	#endif
-
-	//init uart
-	uart_init(UART_BAUD_SELECT(UART_BAUD_RATE,F_CPU));
-
+	
 	//init interrupt
 	sei();
 
@@ -50,105 +31,31 @@ int main(void) {
 	mpu6050_init();
 	_delay_ms(50);
 
-	//init mpu6050 dmp processor
-	#if MPU6050_GETATTITUDE == 2
-	mpu6050_dmpInitialize();
-	mpu6050_dmpEnable();
-	_delay_ms(10);
-	#endif
-
-	for(;;) {
-
-		#if MPU6050_GETATTITUDE == 0
-		mpu6050_getRawData(&ax, &ay, &az, &gx, &gy, &gz);
-		mpu6050_getConvData(&axg, &ayg, &azg, &gxds, &gyds, &gzds);
-		#endif
-
-		#if MPU6050_GETATTITUDE == 1
-		mpu6050_getQuaternion(&qw, &qx, &qy, &qz);
-		mpu6050_getRollPitchYaw(&roll, &pitch, &yaw);
-		_delay_ms(10);
-		#endif
-
-		#if MPU6050_GETATTITUDE == 2
-		if(mpu6050_getQuaternionWait(&qw, &qx, &qy, &qz)) {
-			mpu6050_getRollPitchYaw(qw, qx, qy, qz, &roll, &pitch, &yaw);
-		}
-		_delay_ms(10);
-		#endif
-
-		#if MPU6050_GETATTITUDE == 0
-		char itmp[10];
-		ltoa(ax, itmp, 10); uart_putc(' '); uart_puts(itmp); uart_putc(' ');
-		ltoa(ay, itmp, 10); uart_putc(' '); uart_puts(itmp); uart_putc(' ');
-		ltoa(az, itmp, 10); uart_putc(' '); uart_puts(itmp); uart_putc(' ');
-		ltoa(gx, itmp, 10); uart_putc(' '); uart_puts(itmp); uart_putc(' ');
-		ltoa(gy, itmp, 10); uart_putc(' '); uart_puts(itmp); uart_putc(' ');
-		ltoa(gz, itmp, 10); uart_putc(' '); uart_puts(itmp); uart_putc(' ');
-		uart_puts("\r\n");
-
-		dtostrf(axg, 3, 5, itmp); uart_puts(itmp); uart_putc(' ');
-		dtostrf(ayg, 3, 5, itmp); uart_puts(itmp); uart_putc(' ');
-		dtostrf(azg, 3, 5, itmp); uart_puts(itmp); uart_putc(' ');
-		dtostrf(gxds, 3, 5, itmp); uart_puts(itmp); uart_putc(' ');
-		dtostrf(gyds, 3, 5, itmp); uart_puts(itmp); uart_putc(' ');
-		dtostrf(gzds, 3, 5, itmp); uart_puts(itmp); uart_putc(' ');
-		uart_puts("\r\n");
-
-		uart_puts("\r\n");
-
+	while(1) {
 		/*	 Tänd LED 1 sek	*/
-		double combinedAcc = fabs(axg) + fabs(ayg) + fabs(azg);
+		double combinedAcc = fabs(getAcc(X)) + fabs(getAcc(Y)) + fabs(getAcc(Z));
 		if(combinedAcc > 2.0){
 			PORTB |= (1<<PB0);
 			_delay_ms(1000);
 			PORTB &= ~(1<<PB0);
 		}
 		_delay_ms(1);
-		#endif
-
-		#if MPU6050_GETATTITUDE == 1 || MPU6050_GETATTITUDE == 2
-		//quaternion
-		ptr = (long *)(&qw);
-		uart_putc(*ptr);
-		uart_putc(*ptr>>8);
-		uart_putc(*ptr>>16);
-		uart_putc(*ptr>>24);
-		ptr = (long *)(&qx);
-		uart_putc(*ptr);
-		uart_putc(*ptr>>8);
-		uart_putc(*ptr>>16);
-		uart_putc(*ptr>>24);
-		ptr = (long *)(&qy);
-		uart_putc(*ptr);
-		uart_putc(*ptr>>8);
-		uart_putc(*ptr>>16);
-		uart_putc(*ptr>>24);
-		ptr = (long *)(&qz);
-		uart_putc(*ptr);
-		uart_putc(*ptr>>8);
-		uart_putc(*ptr>>16);
-		uart_putc(*ptr>>24);
-
-		//roll pitch yaw
-		ptr = (long *)(&roll);
-		uart_putc(*ptr);
-		uart_putc(*ptr>>8);
-		uart_putc(*ptr>>16);
-		uart_putc(*ptr>>24);
-		ptr = (long *)(&pitch);
-		uart_putc(*ptr);
-		uart_putc(*ptr>>8);
-		uart_putc(*ptr>>16);
-		uart_putc(*ptr>>24);
-		ptr = (long *)(&yaw);
-		uart_putc(*ptr);
-		uart_putc(*ptr>>8);
-		uart_putc(*ptr>>16);
-		uart_putc(*ptr>>24);
-
-		uart_putc('\n');
-		#endif
-
 	}
 }
+
+double getAcc(int addr){
+	int16_t ret = 0;
+	uint8_t buffer[2];
+	i2c_start(MPU6050_ADDR | I2C_WRITE);
+	i2c_write(addr);
+	_delay_us(10);
+	//read data
+	i2c_start(MPU6050_ADDR | I2C_READ);
+	buffer[0] = i2c_readAck();
+	buffer[1] = i2c_readNak();
+	i2c_stop();
+	ret = (((int16_t)buffer[0]) << 8) | buffer[1];
+	return (double)(ret)/MPU6050_AGAIN; //Konverterar till g?
+}
+
+
