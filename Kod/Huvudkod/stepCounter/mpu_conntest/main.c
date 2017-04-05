331c65065f165f842ecc79d0d948a150839bfee5
Johan Kämpe <kampe.johan@gmail.com>
2017-04-05 09:35:33 +0200

    Updated with comments

--- Kod/Huvudkod/stepCounter/mpu_conntest/main.c
+++ Kod/Huvudkod/stepCounter/mpu_conntest/main.c
@@ -1,3 +1,18 @@
+/*	 stepCounter
+	
+	Project for course "Programmering av inbyggda system"
+		- Mjukvaruutvecklare inbyggda system
+	
+	Get steps with MPU-9250 acceleration data
+	Display step count to display SSD 1306
+	
+	Members:
+	Simon Karlsson
+	Dennis Bunne
+	Johan Kämpe
+	*/
+
+
 #include <stdlib.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -18,44 +33,59 @@
 #define STEP_ACC_TRIGGER 2.3
 
 u8g_t u8g;
+uint16_t steps = 0;
+double accIdle = 0;
+double accCombined = 0;
+double accX = 0.0, accY = 0.0, accZ = 0.0;
 
 double getAcc(int addr);
 void drawSteps(uint16_t steps);
 void drawString(char * string);
 void drawAccData(double x, double y, double z);
-void getAccOffset(double *ox, double *oy, double *oz);
+void setAccIdle();
+double getAccXYZ(void);
 
 int main(void) {
+	/*	 Init MPU 6050	*/
+	sei();
+	mpu6050_init();
+	_delay_ms(50);
+	
 	u8g_InitI2C(&u8g, &u8g_dev_ssd1306_128x64_i2c, U8G_I2C_OPT_NONE);
 	u8g_SetFont(&u8g, u8g_font_fub14);
+	
+	_delay_ms(50);
 	drawString("STEPCOUNTER!");
-	double combinedAcc = 0;
-	double accX = 0.0, accY = 0.0, accZ = 0.0;
-	double xOffset = 0.0, yOffset = 0.0, zOffset = 0.0;
-	uint16_t steps = 0;
-	//getAccOffset(&xOffset, &yOffset, &zOffset);
-	sei();
-	DDRB |= (1 << PB0);
-	/*	 Init MPU 6050	*/
-	//mpu6050_init();
+	
+	setAccIdle();
 	_delay_ms(50);
 
+	DDRB |= (1 << PB0);
+
 	while(1) {
-		accX = getAcc(X) - xOffset; 
-		accY = getAcc(Y) - yOffset;
-		accZ = getAcc(Z) - zOffset;
-		combinedAcc = (accX + accY + accZ);
-		if(combinedAcc > STEP_ACC_TRIGGER){
+		accCombined = getAccXYZ();
+		if(!accCombined){
+			PORTB= 0b00000001;
+			_delay_ms(100);
+			PORTB= 0b00000000;
+			_delay_ms(100);
+		}
+		if(fabs(accCombined - accIdle) > STEP_ACC_TRIGGER){
 			steps++;
 			PORTB= 0b00000001;
 			drawSteps(steps);
 			_delay_ms(50);
 			PORTB= 0b00000000;
 		}
-		//drawSteps(xOffset);
-		drawSteps(steps++);
-		_delay_ms(100);
+		_delay_ms(10);
 	}
+}
+
+double getAccXYZ(void){
+	accX = getAcc(X);
+	accY = getAcc(Y);
+	accZ = getAcc(Z);
+	return (accX + accY + accZ);
 }
 
 double getAcc(int addr){
@@ -69,9 +99,7 @@ double getAcc(int addr){
 	buffer[0] = i2c_readAck();
 	buffer[1] = i2c_readNak();
 	i2c_stop();
-	//ret = (((int16_t)buffer[0]) << 8) | buffer[1];
 	ret = fabs((((int16_t)buffer[0]) << 8) | buffer[1]);
-	//return fabs(ret) / 1000;
 	return (double)(ret)/MPU6050_AGAIN; //Konverterar till g?
 }
 
@@ -91,6 +119,15 @@ void drawString(char * string){
 	}while(u8g_NextPage(&u8g));
 }
 
+void setAccIdle(){
+	for(int i = 0; i < 10; i++){
+		 accIdle += getAccXYZ();  
+		 drawString("setAccIdle");
+		 _delay_ms(100);
+	}
+	accIdle /= 10;
+}
+
 void drawAccData(double x, double y, double z){
 	char ax[5] = "\0";
 	char ay[5] = "\0";
@@ -98,29 +135,10 @@ void drawAccData(double x, double y, double z){
 	dtostrf(x,1,3,ax);
 	dtostrf(y,1,3,ay);
 	dtostrf(z,1,3,az);
-	//itoa(x, ax, 10);
-	//itoa(y, ay, 10);
-	//itoa(z, az, 10);
 	u8g_FirstPage(&u8g);
 	do{
 		u8g_DrawStr(&u8g, 2, 16, ax);
 		u8g_DrawStr(&u8g, 2, 16*2 + 1, ay);
 		u8g_DrawStr(&u8g, 2, 16*3 + 2, az);
 	}while(u8g_NextPage(&u8g));
-}
-
-void getAccOffset(double *ox, double *oy, double *oz){
-	double accX = 0, accY = 0, accZ = 0;
-	drawString("Calibrating...");
-	for(int i = 0; i < OFFSET_NUM; i++){
-		accX += getAcc(X);
-		accY += getAcc(Y); 
-		accZ += getAcc(Z); 
-		_delay_ms(10);
-	}
-	*ox = (double)(accX / OFFSET_NUM);
-	*oy = (double)(accY / OFFSET_NUM);
-	*oz = (double)(accZ / OFFSET_NUM);
-	drawString("Calib. done");
-	_delay_ms(500);
 }
